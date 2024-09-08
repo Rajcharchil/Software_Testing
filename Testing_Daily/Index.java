58682049804b13c8a1a014a2072834d186773d24
charchil raj <114642639+Rajcharchil@users.noreply.github.com>
2024-09-08 16:56:01 +0530

    Add files via upload

---
+++ Testing_Daily/Index.java
@@ -0,0 +1,24 @@
+package java_Day2;
+
+//public class Index {
+//
+//	public static void main(String[] args) {
+//		String text ="Hallo,World";
+//		int index = text.indexOf('W');
+//		System.out.print("index of 'W':"+index);
+//	}
+//
+//}
+
+
+//                 Replace Character
+
+public class Index {
+
+	public static void main(String[] args) {
+		String text ="Charchil how're you.";
+		String replacetext = text.replace("Charchil","lokesh");
+		System.out.print("Replace String:  "+replacetext);
+	}
+
+}
